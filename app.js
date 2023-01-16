c0c5c4343ef998cbbaf7167b5803ae4d87e94313
SebGom1983 <119420661+SebGom1983@users.noreply.github.com>
2023-01-16 09:20:13 -0500

    se modifica app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
+/*
 app.listen(3000,() => {
     console.log('Servidor corriendo en el puerto 3000');
 });
+*/
+
+app.listen(port,() => 
+    console.log('Servidor corriendo en el puerto ${PORT}'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
